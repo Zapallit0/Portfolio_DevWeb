a0db31528171840a5c7b66ec4ff4e892d4f29c0e
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-06-11 09:41:50 -0500

    Phone display done

--- FrontEnd/src/Pages/Home/Contact/Contact.jsx
+++ FrontEnd/src/Pages/Home/Contact/Contact.jsx
@@ -25,35 +25,35 @@ function Contact() {
         </div>
         <div className='contactLink'>
             <ul>
-                <h4>Contacto</h4>
+                <h4>Contacto: </h4>
                 <p>Estamos disponibles para tus consultas, sugerencias usando los siguientes canales de comunicación:</p>
                 <li>
                     <div>
                         <img src={Phone} alt="Telefono" height='30px' width='30px'/>
                     </div>
-                    <h5>Teléfono</h5>
-                    <p>+51 936427966</p>
+                    <h5>Teléfono: </h5>
+                    <a href='tel:+51 936427966'>+51 936427966</a>
                 </li>
                 <li>
                     <div>
                         <img src={Mail} alt="Correo" height='30px' width='30px'/>
                     </div>
-                    <h5>Correo</h5>
-                    <p>contacto@jhersvin.com</p>
+                    <h5>Correo: </h5>
+                    <a href='mailto:contacto@jhersvin.com'>contacto@jhersvin.com</a>
                 </li>
                 <li>
                     <div>
                         <img src={Linkedin} alt="Linkedin"height='30px' width='30px'/>
                     </div>
-                    <h5>LinkedIn</h5>
-                    <p>Jhersvin Villodas</p>
+                    <h5>LinkedIn: </h5>
+                    <a href='https://www.linkedin.com/in/jhersvin-villodas-quinto-98669b233/'>Jhersvin Villodas</a>
                 </li>
                 <li>
                     <div>
                         <img src={X} alt="X" height='30px' width='30px'/>
                     </div>
                     <h5>X/Twitter</h5>
-                    <p>JhersDev</p>
+                    <a href='https://x.com/Zapallito4u'>JhersDev</a>
                 </li>
             </ul>
         </div>
