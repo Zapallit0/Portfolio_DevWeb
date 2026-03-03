d7726c78cd41ab6f26e4a9cc716c2c692ab87815
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-03-03 11:41:25 -0500

    Cambios en el footer y cmbio de logo

--- FrontEnd/src/Pages/Home/Footer/Footer.jsx
+++ FrontEnd/src/Pages/Home/Footer/Footer.jsx
@@ -1,19 +1,33 @@
 import './Footer.css'
 import { Link } from 'react-router-dom'
+import { Github, Linkedin, Instagram } from 'lucide-react'
 import Logo from '../../../assets/Logo/Jhersvin/Logo_Jhersvin.svg'
 import BrochureDR from '../../../PDF/DigitalRescue.pdf'
 import { ROUTES } from '../../../constants/routes'
 
 function Footer() {
   return (
-    <footer>
-        <a href="www.jhersvin.com"><img src={Logo} alt="Logo D" height="100px" width="100px"/></a>
-        <ul>
-            <li><Link to={ROUTES.HOME}>Home</Link></li>
-            <li><Link to={ROUTES.SERVICES}>Servicios</Link></li>
-            <li><Link to={ROUTES.ABOUT}>Nosotros</Link></li>
-            <li><a href={BrochureDR} target='_blank' rel='noopener noreferrer' download>BROCHURE</a></li>
-        </ul>
+    <footer className="footer">
+      <a href="https://www.jhersvin.com" className="footer__logo">
+        <img src={Logo} alt="Logo Jhersvin" height="80" width="80" />
+      </a>
+
+      <ul className="footer__nav">
+        <li><Link to={ROUTES.HOME}>Home</Link></li>
+        <li><Link to={ROUTES.SERVICES}>Servicios</Link></li>
+        <li><Link to={ROUTES.ABOUT}>Nosotros</Link></li>
+        <li><a href={BrochureDR} target="_blank" rel="noopener noreferrer" download>Brochure</a></li>
+      </ul>
+
+      <hr className="footer__divider" />
+
+      <div className="footer__socials">
+        <a href="#" aria-label="GitHub"><Github size={20} /></a>
+        <a href="#" aria-label="LinkedIn"><Linkedin size={20} /></a>
+        <a href="#" aria-label="Instagram"><Instagram size={20} /></a>
+      </div>
+
+      <p className="footer__copy">&copy; 2026 Jhersvin. Todos los derechos reservados.</p>
     </footer>
   )
 }
